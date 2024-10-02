f8db028cca79cff3f610145abc1116996b2788c6
Jamie Jonas <jamie@oneten.tech>
2024-10-02 12:10:07 +0200

    added sample VNET

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "~> 0.3"
+}
 
-module "dummy" {
-  source = "./modules/module-name"
-  name   = var.name
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = module.regions.regions[random_integer.region_index.result].name
+  name     = module.naming.resource_group.name_unique
+}
+
+# Creating a virtual network with a unique name, telemetry settings, and in the specified resource group and location.
+module "vnet" {
+  source              = "Azure/avm-res-network-virtualnetwork/azurerm//examples/default"
+  version             = "0.4.0"
+  name                = module.naming.virtual_network.name
+  enable_telemetry    = true
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  address_space = ["10.0.0.0/16"]
 }
