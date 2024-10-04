95897e689e772588dbd7b7174117388d0c1d26fd
Jamie Jonas <jamie@oneten.tech>
2024-10-04 14:56:19 +0200

    fixed output

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "virtual_network_id" {
   value = module.vnet.resource_id
 }
 output "resource_group_name" {
-  value = azurerm_resource_group.main.name
+  value = module.avm-res-resources-resourcegroup.name
 }
 output "virtual_network_name" {
   value = module.vnet.name
